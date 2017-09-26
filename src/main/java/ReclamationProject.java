6e5746dace5f0821cbf33635fc8b0df94ae7132c
surjo <surjo@PC-de-Surjo>
2017-09-26 14:36:45 -0500

    Lab 4

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -1,4 +1,4 @@
-/*
+/**
  * The following code needs a lot of TLC. So give it some!
  *
  * 1. Fix all checkstyle errors
@@ -7,20 +7,42 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ *
+ */
+public class ReclamationProject {
+    /**
+     * Input two strings and returns longest common substring.
+     * @param x
+     * @param y
+     * @return r
+     */
+    static String longestSubstring(final String x, final String y) {
+        String a = x;
+        String b = y;
+        if (a.length() > b.length()) {
+            String c = a; //assigning value of a to temp variable c.
+            a = b;
+            b = c;
+        }
+        String r = "";
+        if (a.equals(b)) {
+            r = "";
+        } else {
+            r = "";
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+                }
+            } // Ah yeah
+        }
+        return r;
+    }
 }
